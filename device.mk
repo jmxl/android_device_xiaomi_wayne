6d647d134178db3d10c2ef659f5b34e60e349b51
dianlujitao <dianlujitao@lineageos.org>
2017-12-07 22:15:14 +0800

    wayne: Build dummy android.hidl.[base,manager]@1.0
     * Required by Oreo-MR0 blobs
    
    Change-Id: I81a081130869c4d92f30b2674e75bb6b096a7a2c
    Signed-off-by: TH779 <tongsj2333@gmail.com>

--- device.mk
+++ device.mk
@@ -216,6 +216,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.health@2.0-service
 
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.manager@1.0
+
 # IDC
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/idc/uinput-fpc.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/uinput-fpc.idc \
