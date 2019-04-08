35c9114fc54f7bd8fcf0b0e015c100caf6389c78
Bowgo Tsai <bowgotsai@google.com>
2019-04-08 18:28:48 +0800

    wayne: Adding GSI keys
    Change-Id: I112ece91566e6cea066cd637bfed5cae6761ae66
    Signed-off-by: TH779 <tongsj2333@gmail.com>

--- lineage_wayne.mk
+++ lineage_wayne.mk
@@ -25,6 +25,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Inherit some common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Installs gsi keys into ramdisk, to boot a GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
 # Inherit from wayne device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
