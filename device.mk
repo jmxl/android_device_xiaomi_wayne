ed63f578bb7eb39f7e0c7ab36eb9b83474cb5de7
TH779 <tongsj2333@gmail.com>
2019-08-29 13:16:32 +0800

    wayne: Move privapp-permissions-qti.xml to system
    Signed-off-by: TH779 <tongsj2333@gmail.com>

--- device.mk
+++ device.mk
@@ -45,7 +45,7 @@ PRODUCT_ENFORCE_RRO_TARGETS := \
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/privapp-permissions-qti.xml
+    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
